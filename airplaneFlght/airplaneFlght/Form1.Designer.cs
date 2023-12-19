1041ef000aa7d2172da1ef2adc1edd1d82e316c5
inane-phe <127912578+inane-phe@users.noreply.github.com>
2023-12-19 17:05:37 +0800

    Add files via upload

---
+++ airplaneFlght/airplaneFlght/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace airplaneFlght
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Air = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.bob = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Air)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bob)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Air
+            // 
+            this.Air.Image = global::airplaneFlght.Properties.Resources.plane_removeBG;
+            this.Air.Location = new System.Drawing.Point(323, 407);
+            this.Air.Name = "Air";
+            this.Air.Size = new System.Drawing.Size(94, 84);
+            this.Air.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Air.TabIndex = 0;
+            this.Air.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.Image = global::airplaneFlght.Properties.Resources.enemy_removeBG;
+            this.Enemy.Location = new System.Drawing.Point(196, 83);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(100, 70);
+            this.Enemy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Enemy.TabIndex = 1;
+            this.Enemy.TabStop = false;
+            this.Enemy.Visible = false;
+            // 
+            // bob
+            // 
+            this.bob.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bob.Image = global::airplaneFlght.Properties.Resources.bomb_removeBG;
+            this.bob.Location = new System.Drawing.Point(205, 495);
+            this.bob.Name = "bob";
+            this.bob.Size = new System.Drawing.Size(34, 44);
+            this.bob.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bob.TabIndex = 2;
+            this.bob.TabStop = false;
+            this.bob.Visible = false;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("微軟正黑體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(40, 43);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(24, 25);
+            this.score.TabIndex = 3;
+            this.score.Text = "0";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("微軟正黑體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(639, 43);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(24, 25);
+            this.time.TabIndex = 5;
+            this.time.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.TabStop = false;
+            this.button1.Text = "開始";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.bob);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.Air);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Air)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bob)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Air;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.PictureBox bob;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ airplaneFlght/airplaneFlght/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace airplaneFlght
+{
+    public partial class Form1 : Form
+    {
+        static int[] StepX = new int [999];
+        List<PictureBox> bullets = new List<PictureBox>();
+        List<PictureBox> enemy = new List<PictureBox>();
+        int n = 0,Score=0;
+        
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+            this.SetBounds(0, 0, 800, 600);
+            Air.Left = (this.Width - Air.Width) / 2;
+            Air.Top = (this.Height - Air.Height)-50;
+            timer1.Enabled = false;
+            Air.Visible = false;
+            score.Visible = false;
+            time.Visible = false;
+        }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            button1.Visible = false;
+            Air.Visible = true;
+            score.Visible = true;
+            time.Visible = true;
+            timer1.Enabled=true;
+            time.Text = textBox1.Text;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Focus();
+            if (n==0)
+            {
+                newene();
+            }
+            enemov();
+            bobmov();
+            double Time = double.Parse(time.Text);
+            if (Time <= 0)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("GAME OVER");
+                Form1 newWindow = new Form1();
+                newWindow.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                Time -= 0.1;
+                time.Text = Time.ToString("F1"); // 保持一位小數
+            }
+
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                if(isOverlap(bullets[i].Bounds,enemy[n-1].Bounds))
+                {
+                    enemy[n - 1].Hide();
+                    n += 1;
+                    Score += 1;
+                    score.Text = "" + Score.ToString();
+                    newene();
+                    /*timer1.Enabled = false;
+                    MessageBox.Show("GAME OVER");
+                    Form1 newWindow = new Form1();
+                    newWindow.Show();
+
+                    this.Hide();*/
+                } 
+            }
+        }
+        bool isOverlap(Rectangle r1,Rectangle r2)
+        {
+            if(r1.Left>r2.Left && r1.Left>r2.Right)
+            {
+                if (r1.Top > r2.Top && r1.Top <r2.Bottom)
+                {
+                    return true;
+                }
+            }
+        return false;
+        }
+        public void enemov()
+        { 
+            for (int i = 0; i < enemy.Count; i++)
+            {
+                if (enemy[i].Right >= this.ClientSize.Width)
+                {
+                    StepX[i] = -1 * StepX[i];
+                }
+                if (enemy[i].Right <= 0)
+                {
+                    StepX[i] = -1 * StepX[i];
+                }
+                enemy[i].Left += StepX[i];
+            }
+        }
+        public void bobmov()
+        {
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                bullets[i].Top -= 10;
+                if (bullets[i].Top <= 100)
+                {
+                    this.Controls.Remove(bullets[i]);
+                    bullets.RemoveAt(i);
+                    i--;
+                    continue;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Space))
+            {
+                newBullet();
+            }
+            if (e.KeyCode.Equals(Keys.Escape))
+            {
+                Application.Exit();
+            }
+            if (e.KeyCode.Equals(Keys.Left))
+            {
+                Air.Left -= 10;
+            }
+            else if (e.KeyCode.Equals(Keys.Right))
+            {
+                Air.Left += 10;
+            }
+            else if (e.KeyCode.Equals(Keys.Up))
+            {
+                Air.Top -= 10;
+            }
+            else if (e.KeyCode.Equals(Keys.Down))
+            {
+                Air.Top+=10;
+            }
+        }
+        public void newBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = bob.Image;
+            bullet.Size = bob.Size;
+            bullet.Left = (Air.Left + Air.Right - bullet.Width) / 2;
+            bullet.Top = Air.Top;
+
+            this.Controls.Add(bullet);
+            this.Controls.SetChildIndex(bullet, 0);
+            bullet.Visible  = true;
+            bullets.Add(bullet);
+
+        }
+        public void newene()
+        {
+            if (n == 0)
+            {
+                n += 1;
+            }
+            PictureBox ene = new PictureBox();
+            ene.Image = Enemy.Image;
+            ene.Size = Enemy.Size;
+            ene.Left = random.Next(0, this.Width - ene.Width); // X 軸隨機位置
+            ene.Top = Enemy.Top;
+
+            this.Controls.Add(ene);
+            this.Controls.SetChildIndex(ene, 0);
+            ene.Visible = true;
+            enemy.Add(ene);
+            StepX[enemy.Count - 1] = 10;
+        }
+    }
+}
